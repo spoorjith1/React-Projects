ad0e5a003b55b61d3dbbc45de8bb3ab49f25180d
spoorjith1 <spoorjith1@gmail.com>
2026-04-17 10:10:25 +0530

    added .env

--- Movie-Search/frontend/src/services/api.js
+++ Movie-Search/frontend/src/services/api.js
@@ -1,5 +1,5 @@
-const API_KEY = "166dd0384c1c80061e2145200af61908";
-const BASE_URL = "http://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularMovies = async ()=> {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
