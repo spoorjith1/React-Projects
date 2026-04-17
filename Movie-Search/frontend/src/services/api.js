285340a83c778538dfae8df2c3502cd506824c58
spoorjith1 <spoorjith1@gmail.com>
2026-04-17 10:29:40 +0530

    fiexd tmdb auth

--- Movie-Search/frontend/src/services/api.js
+++ Movie-Search/frontend/src/services/api.js
@@ -1,14 +1,31 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL = "https://api.themoviedb.org/3"
+const BASE_URL = "https://api.themoviedb.org/3";
 
-export const getPopularMovies = async ()=> {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data = await response.json()
-    return data.results
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      accept: "application/json"
+    }
+  });
+
+  const data = await response.json();
+  return data.results || [];
 };
 
-export const searchMovies = async (query)=> {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = await response.json()
-    return data.results
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?query=${encodeURIComponent(query)}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        accept: "application/json"
+      }
+    }
+  );
+
+  const data = await response.json();
+  return data.results || [];
 };
